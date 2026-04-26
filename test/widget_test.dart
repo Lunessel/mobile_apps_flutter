cea88b8c143121bcb7b3906e1856caa7bc76b1bd
Lunessel <varloky@gmail.com>
2026-04-26 23:42:34 +0300

    fix login test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile_app/main.dart';
+import 'package:mobile_app/providers/connectivity_provider.dart';
+import 'package:mobile_app/screens/login_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Login screen smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => ConnectivityProvider(),
+        child: const MaterialApp(home: LoginScreen()),
+      ),
+    );
+    await tester.pump();
 
     expect(find.text('Hydro Monitor'), findsOneWidget);
     expect(find.text('Увійти'), findsOneWidget);
